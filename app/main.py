ee5c42fd19f6c707c2eb436fee2d72d4728f14f5
Maksym Dazidov <maksymdazidov@gmail.com>
2025-09-10 20:14:52 +0200

    Car and CarWashStation classes modified №2

--- app/main.py
+++ app/main.py
@@ -12,19 +12,26 @@ class CarWashStation:
         self.count_of_ratings = count_of_ratings
 
     def serve_cars(self, cars: list[Car]) -> float:
+        count = 0
 
-        def wash_single_car() -> int:
-            car.clean_mark = self.clean_power
-            return car.clean_mark
-
-        for car in cars:
+        def wash_single_car(car) -> None:
+            # nonlocal count
             if car.clean_mark < self.clean_power:
-                wash_single_car()
+                car.clean_mark = self.clean_power
+                # count += 1
+            return None
 
         def calculate_washing_price() -> float:
-            return round((car.comfort_class * (self.clean_power - car.clean_mark) * self.average_rating) / self.distance_from_city_center, 1)
+            return (car.comfort_class * (self.clean_power - car.clean_mark) * self.average_rating) / self.distance_from_city_center
 
-        return calculate_washing_price()
+        for car in cars:
+            wash_single_car(car)
+            calculate_washing_price()
+            count += calculate_washing_price()
+
+        return round(count, 1)
 
     def rate_service(self, one_rate: int) -> float:
-        return round((self.average_rating * self.count_of_ratings + one_rate) / (self.count_of_ratings + 1), 1)
+        self.average_rating = round((self.average_rating * self.count_of_ratings + one_rate) / (self.count_of_ratings + 1), 1)
+        self.count_of_ratings += 1
+        return self.average_rating
